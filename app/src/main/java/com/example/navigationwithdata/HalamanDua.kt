23c5d80f10a83326c3d290cd6222af4ca25a56dd
Segun Ajibola <FakriSouyo>
2023-11-15 14:01:16 +0700

    Formatui

--- app/src/main/java/com/example/navigationwithdata/HalamanDua.kt
+++ app/src/main/java/com/example/navigationwithdata/HalamanDua.kt
@@ -4,52 +4,82 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
+import com.example.navigationwithdata.data.OrderUiState
+import com.example.navigationwithdata.ui.theme.komponen.FormatLabelHarga
 
 @Composable
+fun HalamanDua (
+    orderUiState: OrderUiState,
+    onCancelButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val items = listOf(
+        Pair(stringResource(R.string.quantity), orderUiState.jumlah),
+        Pair(stringResource(R.string.flavor), orderUiState.rasa)
+    )
 
-fun HalamanDua(
-    contactUiState: ContactUiState,
-    onBackButtonClicked: () -> Unit,
-
-){
     Column (
-        verticalArrangement = Arrangement.Center,
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+            .padding(dimensionResource(R.dimen.medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
     ) {
-        Text(text = "Nama")
-        Text(text = contactUiState.Nama)
-        Divider()
-        Spacer(modifier = Modifier.padding(16.dp))
-        Text(text = "NoHp")
-        Text(text = contactUiState.Nohp)
-        Divider()
-        Spacer(modifier = Modifier.padding(16.dp))
-        Text(text = "Alamat")
-        Text(text = contactUiState.Alamat)
-        Divider()
-        Spacer(modifier = Modifier.padding(16.dp))
+        items.forEach{ item ->
+            Column {
+                Text(item.first.uppercase())
+                Text(text = item.second.toString(), fontWeight = FontWeight.Bold)
+            }
+
+            Divider(
+                thickness = dimensionResource(R.dimen.large)
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .height(dimensionResource(R.dimen.padding_small))
+            )
+
+            FormatLabelHarga(
+                subtotal = orderUiState.harga,
+                modifier = Modifier.align(Alignment.End)
+            )
+        }
+
         Row (
-            modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
+            modifier = Modifier
+                .weight(1f, false)
+                .padding((dimensionResource(R.dimen.padding_small)))
         ) {
-            Button(onClick = onBackButtonClicked) {
-                Text(text =  stringResource(id = R.string.btn_back))
+            Column (
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+            ) {
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    onClick = {}
+                ) {
+                    Text(stringResource(R.string.send))
+                }
+                OutlinedButton(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    onClick = onCancelButtonClicked
+                ) {
+                    Text(stringResource(R.string.cancel))
+                }
             }
         }
     }
-
-
 }
