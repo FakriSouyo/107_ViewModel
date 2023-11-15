fcef90f2d8c4b8eefbc649cde63f9d91f6871a88
Segun Ajibola <FakriSouyo>
2023-11-15 14:14:18 +0700

    fix

--- app/src/main/java/com/example/navigationwithdata/PengelolaHalaman.kt
+++ app/src/main/java/com/example/navigationwithdata/PengelolaHalaman.kt
@@ -1,57 +1,142 @@
 package com.example.navigationwithdata
 
+
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.navigationwithdata.data.SumberData
+import javax.sql.DataSource
 
 enum class PengelolaHalaman {
-    Formulir,
-    Detail
+    Home,
+    Rasa,
+    Summary
 }
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-
-fun ContactApp(
-    viewModel: ContactViewModel = viewModel(),
-    navController: NavHostController = rememberNavController ()
+fun EsJumboAppBar (
+    bisaNavigasiBack: Boolean,
+    navigasiUp: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    Scaffold { innerPadding ->
-        val uiState by viewModel.stateUI.collectAsState()
-        NavHost(
-            navController = navController,
-            startDestination = PengelolaHalaman.Formulir.name,
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            composable(PengelolaHalaman.Formulir.name) {
-                HalamanSatu(
-                    onSubmitClicked = {
-                        viewModel.setContact(it)
-                        navController.navigate(PengelolaHalaman.Detail.name)
-                    }
-                )
-            }
-            composable(PengelolaHalaman.Detail.name){
-                HalamanDua(contactUiState = uiState,
-                    onBackButtonClicked = {
-                        navController.popBackStack(PengelolaHalaman.Formulir.name, false)
-                    })
+    TopAppBar(
+        title = { Text(stringResource(id = R.string.app_name)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = Modifier,
+        navigationIcon = {
+            if (bisaNavigasiBack) {
+                IconButton(onClick = navigasiUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(id = R.string.back_button)
+                    )
                 }
             }
         }
+    )
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EsJumboApp(
+    viewModel: OrderViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold (
+        topBar = {
+            EsJumboAppBar(
+                bisaNavigasiBack = false,
+                navigasiUp = {}
+            )
+        }
+    ) { innerPadding ->
+        val uiState by viewModel.stateUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = PengelolaHalaman.Home.name,
+            modifier = Modifier
+                .padding(innerPadding)
+        ) {
+            composable(
+                route = PengelolaHalaman.Home.name
+            ) {
+                HalamanHome (
+                    onNextButtonClicked = {
+                        navController.navigate(PengelolaHalaman.Rasa.name)
+                    }
+                )
+            }
+
+            composable(
+                route = PengelolaHalaman.Rasa.name
+            ) {
+                val context = LocalContext.current
+                HalamanSatu(
+                    pilihanRasa = SumberData.flavors.map { id ->
+                        context.resources.getString(id)
+                    },
+                    onSelectionChanged = {
+                        viewModel.setRasa(it)
+                    },
+                    onConfirmButtonClicked = {
+                        viewModel.setJumlah(it)
+                    },
+                    onNextButtonClicked = {
+                        navController.navigate(PengelolaHalaman.Summary.name)
+                    },
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToHome(
+                            viewModel,
+                            navController
+                        )
+                    })
+            }
+
+            composable(
+                route = PengelolaHalaman.Summary.name
+            ) {
+                HalamanDua(
+                    orderUiState = uiState,
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToRasa(navController)
+                    }
+                )
+            }
+        }
     }
+}
 
+private fun cancelOrderAndNavigateToHome (
+    viewModel: OrderViewModel,
+    navController: NavHostController
+) {
+    viewModel.resetOrder()
+    navController.popBackStack(PengelolaHalaman.Home.name, inclusive = false)
+}
 
+private fun cancelOrderAndNavigateToRasa(
+    navController: NavHostController
+) {
+    navController.popBackStack(PengelolaHalaman.Rasa.name, inclusive = false)
+}
